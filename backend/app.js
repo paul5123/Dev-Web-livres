1aabe3bf1fc732036f4d03ae4c9ee7c658e98e34
Paul5123 <paul.guillon17@orange.fr>
2026-08-13 23:15:50 +0200

    Ajout de la supression des données

--- backend/app.js
+++ backend/app.js
@@ -35,6 +35,11 @@ Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
     .catch(error => res.status(400).json({ error }));
 });  
 
+app.delete('/api/books/:id', (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
 
 app.get('/api/books/:id', (req, res, next)=>  {
   Book.findOne({_id: req.params.id})
