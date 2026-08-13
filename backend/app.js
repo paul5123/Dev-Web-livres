3aa76c1a3021734f7fe0f0245c83015ddb4e8e0c
Paul5123 <paul.guillon17@orange.fr>
2026-08-13 16:49:39 +0200

    Ajout  de  MongoDb et mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 const app = express();
 
 app.use(express.json());
