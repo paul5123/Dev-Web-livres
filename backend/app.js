8f2937282a1e7f990d3e5ccb3d06b208d4ad5b54
Paul5123 <paul.guillon17@orange.fr>
2026-08-13 19:36:19 +0200

    Ajout des données des livres par identifiant

--- backend/app.js
+++ backend/app.js
@@ -35,6 +35,12 @@ app.get('/api/books/:id', (req, res, next)=>  {
     .catch(error => res.status(400).json({error}));
 });
 
+app.get('/api/books/:id', (req, res, next)=>  {
+  Book.findOne({_id: req.params.id})
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(400).json({error}));
+});
+
 app.get('/api/books', (req, res, next) => {
   Book.find()
     .then(books => res.status(200).json(books))
