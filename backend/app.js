edff1948e0593c1bec4cdb83b8af4799607fc938
Paul5123 <paul.guillon17@orange.fr>
2026-08-14 11:19:27 +0200

    Ajout du dossier routes et controllers, et changement dans la gestion des routes

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const Book = require ('./models/Book')
+const bookRoutes = require ('./routes/book')
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -19,44 +19,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/books', (req, res, next) => {
-  delete req.body.id;
-  const book = new Book ({
-  ...req.body
- });
- book.save()
- .then(()=> res.status(201).json({message:'Objet enregistré !'}))
- .catch(error => res.status(400).json({error}));
-});
-
-app.put ('/api/books/:id', (req, res, next)=>{
-Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});  
-
-app.delete('/api/books/:id', (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/books/:id', (req, res, next)=>  {
-  Book.findOne({_id: req.params.id})
-    .then(book => res.status(200).json(book))
-    .catch(error => res.status(400).json({error}));
-});
-
-app.get('/api/books/:id', (req, res, next)=>  {
-  Book.findOne({_id: req.params.id})
-    .then(book => res.status(200).json(book))
-    .catch(error => res.status(400).json({error}));
-});
-
-app.get('/api/books', (req, res, next) => {
-  Book.find()
-    .then(books => res.status(200).json(books))
-    .catch(error => res.status(400).json({error}));
-});
+app.use('/api/books', bookRoutes);
 
 module.exports = app;
---
+++ backend/routes/book.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+const router = express.Router();
+
+const booksCtrl = require ('../controllers/book');
+router.post('/', booksCtrl.createBook );
+router.put ('/:id',booksCtrl.modifyBook);  
+router.delete('/:id', booksCtrl.deleteBook);
+router.get('/:id', booksCtrl.getOneBook);
+router.get('/',booksCtrl.getAllBook);
+
+module.exports = router;
