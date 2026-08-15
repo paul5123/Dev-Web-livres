eb1506e58ed90dcfa4479149abef89b60e853b87
Paul5123 <paul.guillon17@orange.fr>
2026-08-15 11:01:34 +0200

    Ajout du middleware multer

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,20 +1,24 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const user = require ('../models/User');
+const User = require ('../models/User');
 
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
-      const User = new User({
+      const user = new User({
         email: req.body.email,
         password: hash
       });
+
       user.save()
         .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
         .catch(error => res.status(400).json({ error }));
     })
-    .catch(error => res.status(500).json({ error }));
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error });
+    });
 };
 
 exports.login = (req, res, next) => {
