5d3e08bdffde18852b7bfe34b24627bd06a74401
Paul5123 <paul.guillon17@orange.fr>
2026-08-17 11:47:33 +0200

    Configuration Lf line

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -20,7 +20,7 @@ exports.createBook = (req, res, next) => {
 
 exports.modifyBook = (req, res, next) => {
    const bookObject = req.file ? {
-       ...JSON.parse(req.body.thing),
+       ...JSON.parse(req.body.book),
        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
    } : { ...req.body };
  
